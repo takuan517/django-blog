318a5517c7190297a307f7c4af4829f5e5b3ba15
takuan517 <ishitaku-2000517@keio.jp>
2020-03-28 01:19:16 +0900

    firstcommit

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+# blog/admin.py
+
+from django.contrib import admin
+
+from blog.models import Category, Tag, Post
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post)
