59aab46e541e8885d0f98ab9308a16eaa949bef1
takuan517 <ishitaku-2000517@keio.jp>
2020-03-29 23:38:26 +0900

    コメント機能実装

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,11 @@ from django.db.models import Count, Q
 from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.shortcuts import get_object_or_404
-from blog.models import Post, Category, Tag
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+from django.shortcuts import get_object_or_404, redirect
+from blog.forms import CommentForm, ReplyForm
+from blog.models import Post, Category, Tag, Comment, Reply
 
 class CategoryPostView(ListView):
     model = Post
@@ -58,3 +61,91 @@ class CategoryListView(ListView):
 class TagListView(ListView):
     queryset = Tag.objects.annotate(num_posts=Count(
         'post', filter=Q(post__is_public=True)))
+
+class SearchPostView(ListView):
+    model = Post
+    template_name = 'blog/search_post.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        query = self.request.GET.get('q', None)
+        lookups = (
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(category__name__icontains=query) |
+            Q(tags__name__icontains=query)
+        )
+        if query is not None:
+            qs = super().get_queryset().filter(lookups).distinct()
+            return qs
+        qs = super().get_queryset()
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        return context
+
+class CommentFormView(CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        post_pk = self.kwargs['pk']
+        comment.post = get_object_or_404(Post, pk=post_pk)
+        comment.save()
+        return redirect('blog:post_detail', pk=post_pk)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post_pk = self.kwargs['pk']
+        context['post'] = get_object_or_404(Post, pk=post_pk)
+        return context
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('blog:post_detail', pk=comment.post.pk)
+ 
+ 
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('blog:post_detail', pk=comment.post.pk)
+
+
+class ReplyFormView(CreateView):
+    model = Reply
+    form_class = ReplyForm
+
+    def form_valid(self, form):
+        reply = form.save(commit=False)
+        comment_pk = self.kwargs['pk']
+        reply.comment = get_object_or_404(Comment, pk=comment_pk)
+        reply.save()
+        return redirect('blog:post_detail', pk=reply.comment.post.pk)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        comment_pk = self.kwargs['pk']
+        context['comment'] = get_object_or_404(Comment, pk=comment_pk)
+        return context
+
+
+@login_required
+def reply_approve(request, pk):
+    reply = get_object_or_404(Reply, pk=pk)
+    reply.approve()
+    return redirect('blog:post_detail', pk=reply.comment.post.pk)
+
+
+@login_required
+def reply_remove(request, pk):
+    reply = get_object_or_404(Reply, pk=pk)
+    reply.delete()
+    return redirect('blog:post_detail', pk=reply.comment.post.pk)
